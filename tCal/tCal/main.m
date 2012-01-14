1a722af7daafd1bdc065ba2dd14e4ab35e6cc86d
Michael Crosby <crosby.michael@gmail.com>
2012-01-14 17:49:29 -0500

    Inital commit of project

---
+++ tCal/tCal/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  tCal
+//
+//  Created by Michael Crosby on 1/14/12.
+//  Copyright (c) 2012 Michael Crosby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        
+    }
+    return 0;
+}
+
